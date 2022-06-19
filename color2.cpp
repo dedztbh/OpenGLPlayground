313380cbd6016204cc4976d14e57b59c6e766b48
DE_DZ_TBH <peiqial@outlook.com>
2022-06-19 17:23:05 -0400

    render to texture

--- color2.cpp
+++ color2.cpp
@@ -3,38 +3,13 @@
 
 #include <iostream>
 #include <filesystem>
-#include <vector>
 #include <cmath>
 
-int main() {
-    std::cout << "Current working directory: " << std::filesystem::current_path() << std::endl;
+GLuint MatrixID;
+unsigned int program;
+unsigned int VAO;
 
-    // glfw: initialize and configure
-    // ------------------------------
-    if (!glfwInit()) {
-        fprintf(stderr, "ERROR: could not start GLFW3\n");
-        return 1;
-    }
-
-    // glfw window creation
-    // --------------------
-    GLFWwindow *window = glfwCreateWindow(1000, 1000, "OpenGLPlayground", nullptr, nullptr);
-    if (!window) {
-        std::cout << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
-    glfwMakeContextCurrent(window);
-
-    // start GLEW extension handler
-    glewExperimental = GL_TRUE;
-    glewInit();
-
-    // Enable depth test
-    glEnable(GL_DEPTH_TEST);
-    // Accept fragment if it closer to the camera than the former one
-    glDepthFunc(GL_LESS);
-
+void setup_cube() {
     // shaders
     const char *vertexShaderSource = R"###(
             #version 300 es
@@ -65,7 +40,7 @@ int main() {
     glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
     glCompileShader(fragmentShader);
 
-    unsigned int program = glCreateProgram();
+    program = glCreateProgram();
     glBindAttribLocation(program, 0, "pos");
     glBindAttribLocation(program, 1, "color");
     glAttachShader(program, vertexShader);
@@ -78,90 +53,90 @@ int main() {
     glUseProgram(program);
 
     // Get a handle for our "MVP" uniform
-    GLuint MatrixID = glGetUniformLocation(program, "MVP");
+    MatrixID = glGetUniformLocation(program, "MVP");
 
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
     float vertices[] = {
-            -1.0f,-1.0f,-1.0f, // triangle 1 : begin
-            -1.0f,-1.0f, 1.0f,
+            -1.0f, -1.0f, -1.0f, // triangle 1 : begin
+            -1.0f, -1.0f, 1.0f,
             -1.0f, 1.0f, 1.0f, // triangle 1 : end
-            1.0f, 1.0f,-1.0f, // triangle 2 : begin
-            -1.0f,-1.0f,-1.0f,
-            -1.0f, 1.0f,-1.0f, // triangle 2 : end
-            1.0f,-1.0f, 1.0f,
-            -1.0f,-1.0f,-1.0f,
-            1.0f,-1.0f,-1.0f,
-            1.0f, 1.0f,-1.0f,
-            1.0f,-1.0f,-1.0f,
-            -1.0f,-1.0f,-1.0f,
-            -1.0f,-1.0f,-1.0f,
+            1.0f, 1.0f, -1.0f, // triangle 2 : begin
+            -1.0f, -1.0f, -1.0f,
+            -1.0f, 1.0f, -1.0f, // triangle 2 : end
+            1.0f, -1.0f, 1.0f,
+            -1.0f, -1.0f, -1.0f,
+            1.0f, -1.0f, -1.0f,
+            1.0f, 1.0f, -1.0f,
+            1.0f, -1.0f, -1.0f,
+            -1.0f, -1.0f, -1.0f,
+            -1.0f, -1.0f, -1.0f,
             -1.0f, 1.0f, 1.0f,
-            -1.0f, 1.0f,-1.0f,
-            1.0f,-1.0f, 1.0f,
-            -1.0f,-1.0f, 1.0f,
-            -1.0f,-1.0f,-1.0f,
+            -1.0f, 1.0f, -1.0f,
+            1.0f, -1.0f, 1.0f,
+            -1.0f, -1.0f, 1.0f,
+            -1.0f, -1.0f, -1.0f,
             -1.0f, 1.0f, 1.0f,
-            -1.0f,-1.0f, 1.0f,
-            1.0f,-1.0f, 1.0f,
+            -1.0f, -1.0f, 1.0f,
+            1.0f, -1.0f, 1.0f,
             1.0f, 1.0f, 1.0f,
-            1.0f,-1.0f,-1.0f,
-            1.0f, 1.0f,-1.0f,
-            1.0f,-1.0f,-1.0f,
+            1.0f, -1.0f, -1.0f,
+            1.0f, 1.0f, -1.0f,
+            1.0f, -1.0f, -1.0f,
             1.0f, 1.0f, 1.0f,
-            1.0f,-1.0f, 1.0f,
+            1.0f, -1.0f, 1.0f,
             1.0f, 1.0f, 1.0f,
-            1.0f, 1.0f,-1.0f,
-            -1.0f, 1.0f,-1.0f,
+            1.0f, 1.0f, -1.0f,
+            -1.0f, 1.0f, -1.0f,
             1.0f, 1.0f, 1.0f,
-            -1.0f, 1.0f,-1.0f,
+            -1.0f, 1.0f, -1.0f,
             -1.0f, 1.0f, 1.0f,
             1.0f, 1.0f, 1.0f,
             -1.0f, 1.0f, 1.0f,
-            1.0f,-1.0f, 1.0f
+            1.0f, -1.0f, 1.0f
     };
 
     // One color for each vertex. They were generated randomly.
     static const GLfloat g_color_buffer_data[] = {
-            0.583f,  0.771f,  0.014f,
-            0.609f,  0.115f,  0.436f,
-            0.327f,  0.483f,  0.844f,
-            0.822f,  0.569f,  0.201f,
-            0.435f,  0.602f,  0.223f,
-            0.310f,  0.747f,  0.185f,
-            0.597f,  0.770f,  0.761f,
-            0.559f,  0.436f,  0.730f,
-            0.359f,  0.583f,  0.152f,
-            0.483f,  0.596f,  0.789f,
-            0.559f,  0.861f,  0.639f,
-            0.195f,  0.548f,  0.859f,
-            0.014f,  0.184f,  0.576f,
-            0.771f,  0.328f,  0.970f,
-            0.406f,  0.615f,  0.116f,
-            0.676f,  0.977f,  0.133f,
-            0.971f,  0.572f,  0.833f,
-            0.140f,  0.616f,  0.489f,
-            0.997f,  0.513f,  0.064f,
-            0.945f,  0.719f,  0.592f,
-            0.543f,  0.021f,  0.978f,
-            0.279f,  0.317f,  0.505f,
-            0.167f,  0.620f,  0.077f,
-            0.347f,  0.857f,  0.137f,
-            0.055f,  0.953f,  0.042f,
-            0.714f,  0.505f,  0.345f,
-            0.783f,  0.290f,  0.734f,
-            0.722f,  0.645f,  0.174f,
-            0.302f,  0.455f,  0.848f,
-            0.225f,  0.587f,  0.040f,
-            0.517f,  0.713f,  0.338f,
-            0.053f,  0.959f,  0.120f,
-            0.393f,  0.621f,  0.362f,
-            0.673f,  0.211f,  0.457f,
-            0.820f,  0.883f,  0.371f,
-            0.982f,  0.099f,  0.879f
+            0.583f, 0.771f, 0.014f,
+            0.609f, 0.115f, 0.436f,
+            0.327f, 0.483f, 0.844f,
+            0.822f, 0.569f, 0.201f,
+            0.435f, 0.602f, 0.223f,
+            0.310f, 0.747f, 0.185f,
+            0.597f, 0.770f, 0.761f,
+            0.559f, 0.436f, 0.730f,
+            0.359f, 0.583f, 0.152f,
+            0.483f, 0.596f, 0.789f,
+            0.559f, 0.861f, 0.639f,
+            0.195f, 0.548f, 0.859f,
+            0.014f, 0.184f, 0.576f,
+            0.771f, 0.328f, 0.970f,
+            0.406f, 0.615f, 0.116f,
+            0.676f, 0.977f, 0.133f,
+            0.971f, 0.572f, 0.833f,
+            0.140f, 0.616f, 0.489f,
+            0.997f, 0.513f, 0.064f,
+            0.945f, 0.719f, 0.592f,
+            0.543f, 0.021f, 0.978f,
+            0.279f, 0.317f, 0.505f,
+            0.167f, 0.620f, 0.077f,
+            0.347f, 0.857f, 0.137f,
+            0.055f, 0.953f, 0.042f,
+            0.714f, 0.505f, 0.345f,
+            0.783f, 0.290f, 0.734f,
+            0.722f, 0.645f, 0.174f,
+            0.302f, 0.455f, 0.848f,
+            0.225f, 0.587f, 0.040f,
+            0.517f, 0.713f, 0.338f,
+            0.053f, 0.959f, 0.120f,
+            0.393f, 0.621f, 0.362f,
+            0.673f, 0.211f, 0.457f,
+            0.820f, 0.883f, 0.371f,
+            0.982f, 0.099f, 0.879f
     };
 
-    unsigned int VBO, VAO, colorbuffer;
+    unsigned int VBO, colorbuffer;
     glGenVertexArrays(1, &VAO);
     glBindVertexArray(VAO);
     glGenBuffers(1, &VBO);
@@ -176,6 +151,174 @@ int main() {
     glEnableVertexAttribArray(1);
     glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+}
+
+int main() {
+    std::cout << "Current working directory: " << std::filesystem::current_path() << std::endl;
+
+    // glfw: initialize and configure
+    // ------------------------------
+    if (!glfwInit()) {
+        fprintf(stderr, "ERROR: could not start GLFW3\n");
+        return 1;
+    }
+
+    // glfw window creation
+    // --------------------
+    GLFWwindow *window = glfwCreateWindow(1000, 1000, "OpenGLPlayground", nullptr, nullptr);
+    if (!window) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    // start GLEW extension handler
+    glewExperimental = GL_TRUE;
+    glewInit();
+
+    // Enable depth test
+    glEnable(GL_DEPTH_TEST);
+    // Accept fragment if it closer to the camera than the former one
+    glDepthFunc(GL_LESS);
+
+    setup_cube();
+
+    /**
+     * Render to Texture
+     * */
+
+    // shaders
+    const char *vertexShaderSource = R"###(
+            #version 300 es
+            out highp vec2 texCoord;
+            in highp vec3 pos;
+            in highp vec2 aTexCoord;
+
+            void main() {
+                gl_Position = vec4(pos, 1.0);
+                texCoord = aTexCoord;
+            }
+    )###";
+
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
+    glCompileShader(vertexShader);
+
+    const char *fragmentShaderSource = R"###(
+            #version 300 es
+            out lowp vec4 fragColor;
+            in highp vec2 texCoord;
+            uniform sampler2D t;
+
+            void main() {
+                fragColor = texture(t, texCoord);
+            }
+    )###";
+
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
+    glCompileShader(fragmentShader);
+
+    unsigned int program2 = glCreateProgram();
+    glBindAttribLocation(program2, 0, "pos");
+    glBindAttribLocation(program2, 1, "aTexCoord");
+    glAttachShader(program2, vertexShader);
+    glAttachShader(program2, fragmentShader);
+    glBindFragDataLocation(program2, 0, "fragColor");
+    glLinkProgram(program2);
+    glUseProgram(program2);
+
+    GLuint FBO = 0;
+    glGenFramebuffers(1, &FBO);
+    glBindFramebuffer(GL_FRAMEBUFFER, FBO);
+
+    // The texture we're going to render to
+    GLuint renderedTexture;
+    glGenTextures(1, &renderedTexture);
+
+    // "Bind" the newly created texture : all future texture functions will modify this texture
+    glBindTexture(GL_TEXTURE_2D, renderedTexture);
+
+    // Give an empty image to OpenGL ( the last "0" )
+    int width = 256;
+    int height = 256;
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, nullptr);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    // The depth buffer
+    GLuint depthrenderbuffer;
+    glGenRenderbuffers(1, &depthrenderbuffer);
+    glBindRenderbuffer(GL_RENDERBUFFER, depthrenderbuffer);
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, width, height);
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, depthrenderbuffer);
+
+    // Set "renderedTexture" as our colour attachement #0
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, renderedTexture, 0);
+
+//    // Set the list of draw buffers.
+//    GLenum DrawBuffers[1] = {GL_COLOR_ATTACHMENT0};
+//    glDrawBuffers(1, DrawBuffers); // "1" is the size of DrawBuffers
+
+    // Always check that our framebuffer is ok
+    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+        return 166;
+
+    // set up vertex data (and buffer(s)) and configure vertex attributes
+    // ------------------------------------------------------------------
+    float vertices[] = {
+            1.0f, 1.0f, 0.0f,
+            1.0f, 0.0f, 0.0f,
+            0.0f, 0.0f, 0.0f,
+            0.0f, 1.0f, 0.0f,
+            0.0f, 1.0f, 0.0f,
+            0.0f, 0.0f, 0.0f,
+            -1.0f, 0.0f, 0.0f,
+            -1.0f, 1.0f, 0.0f,
+    };
+    float texCoords[] = {
+            1.0f, 1.0f,
+            1.0f, 0.0f,
+            0.0f, 0.0f,
+            0.0f, 1.0f,
+            1.0f, 1.0f,
+            1.0f, 0.0f,
+            0.0f, 0.0f,
+            0.0f, 1.0f
+    };
+    unsigned int indices[] = {
+            0, 1, 3, // first triangle
+            1, 2, 3,  // second triangle
+            4, 5, 7, // first triangle
+            5, 6, 7,  // second triangle
+    };
+
+    unsigned int VBO2, VAO2, EBO2, TEXCOORD;
+    glGenVertexArrays(1, &VAO2);
+    glGenBuffers(1, &VBO2);
+    glGenBuffers(1, &EBO2);
+    glGenBuffers(1, &TEXCOORD);
+
+    glBindVertexArray(VAO2);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO2);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO2);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ARRAY_BUFFER, TEXCOORD);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(texCoords), texCoords, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO2);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+    glEnableVertexAttribArray(1);
+    glBindBuffer(GL_ARRAY_BUFFER, TEXCOORD);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), nullptr);
+
 
     // render loop
     // -----------
@@ -184,26 +327,42 @@ int main() {
     while (!glfwWindowShouldClose(window)) {
         double currentTime = glfwGetTime();
         nbFrames++;
-        if ( currentTime - lastTime >= 1.0 ){ // If last prinf() was more than 1 sec ago
+        if (currentTime - lastTime >= 1.0) { // If last prinf() was more than 1 sec ago
             // printf and reset timer
             printf("%f fps\n", double(nbFrames));
             nbFrames = 0;
             lastTime += 1.0;
         }
 
-        double diff = (currentTime - (int)currentTime) * 2 * M_PI;
+        glBindFramebuffer(GL_FRAMEBUFFER, FBO);
+        glViewport(0, 0, width, height);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        glUseProgram(program);
+
+        double diff = (currentTime - (int) currentTime) * 2 * M_PI;
         glPushMatrix();
         glLoadIdentity();
         gluPerspective(45.0f, 1.0f, 0.1f, 100.0f);
-        gluLookAt(4 * sin(diff),3,-4 * cos(diff), 0, 0, 0, 0, 1, 0);
-        glUseProgram(program);
+        gluLookAt(4 * sin(diff), 3, -4 * cos(diff), 0, 0, 0, 0, 1, 0);
+
         glBindVertexArray(VAO);
         float MVP[16];
         glGetFloatv(GL_MODELVIEW_MATRIX, MVP);
         glUniformMatrix4fv(MatrixID, 1, GL_FALSE, MVP);
         glPopMatrix();
+        glDrawArrays(GL_TRIANGLES, 0, 12 * 3);
+
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
+        glViewport(0, 0, 1000, 1000);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        glDrawArrays(GL_TRIANGLES, 0, 12*3);
+        glUseProgram(program2);
+        glBindVertexArray(VAO2);
+        glBindTexture(GL_TEXTURE_2D, renderedTexture);
+        glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, nullptr);
+
+        glUseProgram(0);
+        glBindVertexArray(0);
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
@@ -214,7 +373,7 @@ int main() {
     // optional: de-allocate all resources once they've outlived their purpose:
     // ------------------------------------------------------------------------
     glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
+//    glDeleteBuffers(1, &VBO);
 
     // glfw: terminate, clearing all previously allocated GLFW resources.
     // ------------------------------------------------------------------
